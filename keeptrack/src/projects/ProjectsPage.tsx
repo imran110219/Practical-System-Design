848641618ef868e3216cfcf6b5d24c160fcd2b1a
Sadman <imran@Sadmans-MacBook-Pro.local>
2023-07-02 21:14:18 +0600

    Lab C complete

--- keeptrack/src/projects/ProjectsPage.tsx
+++ keeptrack/src/projects/ProjectsPage.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { MOCK_PROJECTS } from "./MockProjects";
+import ProjectList from "./ProjectList";
+import { Project } from "./Project";
 
 function ProjectsPage() {
-    return (
-        <>
-            <h1>Projects</h1>
-            <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
-        </>
-    );
+  const saveProject = (project: Project) => {
+    console.log("Saving project: ", project);
+  };
+  return (
+    <>
+      <h1>Projects</h1>
+      <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} />
+    </>
+  );
 }
 
 export default ProjectsPage;
